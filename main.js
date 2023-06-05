ada06f5d527f4f68a223680d0d125438db41a388
arief294 <135583513+arief294@users.noreply.github.com>
2023-06-05 16:33:27 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const About = document.getElementById("About");
+const Contact = document.getElementById("Contact");
+
+About.addEventListener("click", function () {
+    window.location.href = "Portofolio.html";
+});
+Contact.addEventListener("click", function () {
+    window.location.href = "Muter.html";
+});
